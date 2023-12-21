72f90c01973e93a2af34700aec78e525841bfb91
rohithvankadaru <rohithvankadaru405@gmail.com>
2023-12-21 11:33:46 +0530

    khsduiekn

--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -1,14 +1,29 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import './note.css'
 import { MdOutlineEdit } from "react-icons/md"
 import { RiDeleteBin5Line } from "react-icons/ri"
 import { IoIosColorPalette } from "react-icons/io";
 import { TbLetterA } from "react-icons/tb";
+import { Modal } from 'antd';
 
-const Note = ({ title, text, index, editText, deleteFun}) => {
+const Note = ({ title, text, index, editText, deleteFun }) => {
 
   const editSpanRef = useRef(null);
   const deleteSpanRef = useRef(null);
+  const [showModal, setShowModal] = useState(false);
+
+  function handleDelete() {
+    setShowModal(true);
+  }
+
+  function handleOk() {
+    deleteFun(deleteSpanRef);
+    setShowModal(false);
+  }
+
+  function handleCancel() {
+    setShowModal(false);
+  }
 
   return (
     <>
@@ -16,7 +31,7 @@ const Note = ({ title, text, index, editText, deleteFun}) => {
         <h3>{title}</h3>
         <div className='note-body'>{text}</div>
         <span className='edit-icon-wrapper icon-wrapper' index={index} ref={editSpanRef} onClick={() => editText(editSpanRef)}><MdOutlineEdit /></span>
-        <span className='delete-icon-wrapper icon-wrapper' index={index} ref={deleteSpanRef} onClick={() => deleteFun(deleteSpanRef)}><RiDeleteBin5Line /></span>
+        <span className='delete-icon-wrapper icon-wrapper' index={index} ref={deleteSpanRef} onClick={handleDelete}><RiDeleteBin5Line /></span>
         {/* <span className='color-picer-wrapper icon-wrapper' index={index}>
           <IoIosColorPalette />
           <input type="color" />
@@ -26,6 +41,9 @@ const Note = ({ title, text, index, editText, deleteFun}) => {
           <input type="color" />
         </span> */}
       </div>
+      <Modal open={showModal} onOk={handleOk} onCancel={handleCancel}>
+        <label>Are you sure, you want to delete</label>
+      </Modal>
     </>
   )
 }
