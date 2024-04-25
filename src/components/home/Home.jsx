1e6178c195997683bf3c906d614569d29427fa76
rohithvankadaru <121030768+rohithvankadaru@users.noreply.github.com>
2024-04-25 07:38:57 +0000

    changed placing of add note icon

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -182,7 +182,7 @@ const Home = () => {
         setSearchText={setSearchText}
         deteleAllNotes={deteleAllNotes} />
       <div className='home'>
-        <div style={{ cursor: 'pointer'}} className='d-flex flex-col align-center'><img src={note_icon} className='plus-icon' style={{ width: '35px' }} onClick={showModal} /><span style={{ fontSize: '10px' }}>Add Note</span></div>
+        <div style={{ cursor: 'pointer', position: 'fixed', right: '50px', bottom: '50px', backgroundColor: 'white'}} className='d-flex flex-col align-center'><img src={note_icon} className='plus-icon' style={{ width: '60px' }} onClick={showModal} /><span style={{ fontSize: '12px' }}>Add Note</span></div>
         <span className='bulb-icon-wrapper' ref={bulbIconRef}>
           <PiLightbulbThin className='bulb-icon' />
           <div className='hero-text'> 😊Make your first Note !!</div>
