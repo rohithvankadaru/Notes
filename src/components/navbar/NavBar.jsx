0f4d9ba7dc4bf1577e4a682b15d3622c2e36505f
rohithvankadaru <121030768+rohithvankadaru@users.noreply.github.com>
2024-11-23 14:05:50 +0000

    working on options dropdown

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -6,23 +6,37 @@ import { Tooltip } from 'antd';
 
 const NavBar = ({ searchText, setSearchText, deteleAllNotes, isListView, switchLayout }) => {
     const searchBarRef = useRef(null);
+
+    const [dropDownVisible, setDropDownVisible] = useState(false);
     return (
         <div className='nav-bar-wrapper'>
             <div className='d-flex flex-row nav-bar'>
                 <div className='d-flex icon'>
                     <img className="gb_Hc gb_Hd" src="https://cdn-icons-png.freepik.com/256/13454/13454799.png?semt=ais_hybrid"
-                    alt="" aria-hidden="true" role="presentation"></img>
-                    <h1 style={{fontFamily: "Arial", fontSize: '1.85rem'}}>Sticky Note</h1>
+                        alt="" aria-hidden="true" role="presentation"></img>
+                    <h1 style={{ fontFamily: "Arial", fontSize: '1.85rem' }}>Sticky Note</h1>
                 </div>
                 <div className='d-flex search-wrapper'>
                     <CiSearch className='searchIcon' onClick={() => searchBarRef.current.focus()} />
                     <input ref={searchBarRef} placeholder='search...' value={searchText} onChange={e => setSearchText(e.target.value)} />
                 </div>
-                {(isListView && <Tooltip title='Grid view' color='orange'><IoGridOutline onClick={switchLayout} className='view-layout-icon' /></Tooltip>) || (<Tooltip title='List view' color='orange'><CiGrid2H onClick={switchLayout} className='view-layout-icon' /></Tooltip>)}
-                <button onClick={deteleAllNotes} className='clear-btn' style={{ backgroundColor: 'white', boxShadow: '0 0 3px', outline: 'none' }}>Delete All</button>
+                <div
+                 onMouseEnter={() => setDropDownVisible(!dropDownVisible)} onMouseLeave={() => setDropDownVisible(!dropDownVisible)}
+                 >
+                <img className='optionsBurger' style={{ width: '60px' }} src="https://cdn.icon-icons.com/icons2/3106/PNG/512/menu_burger_icon_191633.png" alt="" />
+                <div className='dropDown' style={{display: ((dropDownVisible && 'flex') || 'none')}}>
+                    <button
+                        onClick={deteleAllNotes}
+                        className='clear-btn'>
+                            <img src="https://cdn-icons-png.flaticon.com/512/8256/8256069.png" alt=""  style={{width: '30px'}}/>
+                        Delete All</button>
+                        <div className='changeViewBtn' onClick={switchLayout}> {(isListView && <Tooltip title='Grid view' color='orange'><IoGridOutline  className='view-layout-icon' /></Tooltip>) || (<Tooltip title='List view' color='orange'><CiGrid2H className='view-layout-icon' /></Tooltip>)}
+                        view</div>
+                </div>
+                </div>
             </div>
-            <hr/> 
-        </div> 
+            <hr />
+        </div>
     )
 }
 
