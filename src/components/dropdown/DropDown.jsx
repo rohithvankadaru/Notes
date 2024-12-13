0957cb91cf164bc3af56fb824702af67d8926a40
rohithvankadaru <121030768+rohithvankadaru@users.noreply.github.com>
2024-12-13 13:08:24 +0000

    changed bulb icon, view changes and drop down

--- src/components/dropdown/DropDown.jsx
+++ src/components/dropdown/DropDown.jsx
@@ -1,13 +1,11 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useState } from 'react';
 import './dropDown.css'
-import { CiGrid2H } from 'react-icons/ci'
-import { IoGridOutline } from "react-icons/io5";
 
 
 
 const DropDown = ({ buttonsList }) => {
 
-    const [dropDownVisible, setDropDownVisible] = useState(true);
+    const [dropDownVisible, setDropDownVisible] = useState(false);
 
     function burgerHover ()  {
         setDropDownVisible(!dropDownVisible);
@@ -19,7 +17,7 @@ const DropDown = ({ buttonsList }) => {
 
 
             <div className='dropDownList' 
-            // style={{display: (dropDownVisible && 'flex') || 'none'}}
+            style={{display: (dropDownVisible && 'flex') || 'none'}}
             >
                 {
                     buttonsList.map(({ name, onClickFun }) => {
@@ -28,24 +26,6 @@ const DropDown = ({ buttonsList }) => {
                 }
             </div>
         </div>
-
-        //  <div
-        //     onMouseEnter={() => setDropDownVisible(!dropDownVisible)} onMouseLeave={() => setDropDownVisible(!dropDownVisible)} className='dropDown'
-        // >
-        //     <img className='optionsBurger' style={{ width: '60px' }} src="https://cdn.icon-icons.com/icons2/3106/PNG/512/menu_burger_icon_191633.png" />
-        //     <div className='dropDown-list' style={{ display: ((dropDownVisible && 'block') || 'none') }}>
-        //         <div
-        //             onClick={deteleAllNotes}
-        //             className='deleteAll-btn dropDown-item'>
-        //             <img src="https://cdn-icons-png.flaticon.com/512/8256/8256069.png" alt="" />
-        //             <p>Delete All</p>
-        //         </div>
-        //         <div className='changeViewBtn dropDown-item' onClick={switchLayout}>
-        //             {(isListView && <IoGridOutline className='view-layout-icon' />) || (<CiGrid2H className='view-layout-icon' />)}
-        //             <p>view</p>
-        //         </div>
-        //     </div>
-        // </div> 
     )
 };
 
