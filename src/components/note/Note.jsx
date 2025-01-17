6cff4325f0507d297ba29ff1733a89dea8cbd752
rohithvankadaru <121030768+rohithvankadaru@users.noreply.github.com>
2025-01-17 07:06:51 +0000

    css change in icons container

--- src/components/note/Note.jsx
+++ src/components/note/Note.jsx
@@ -14,9 +14,9 @@ const Note = ({ title, text, index, editText, deleteFun, bgColor, editColor, hig
   const titleRef = useRef(null);
   const [showModal, setShowModal] = useState(false);
 
-  
+
   useEffect(() => {
-    if(highlightText) {
+    if (highlightText) {
       const ranges = findIndeces(text.toUpperCase(), highlightText.toUpperCase());
       text = addHAtRanges(text, ranges);
 
@@ -31,20 +31,20 @@ const Note = ({ title, text, index, editText, deleteFun, bgColor, editColor, hig
     let arr = str.split('');
 
     for (let range of ranges) {
-        let [start, end] = range;
-        arr[start] = '<mark>' + arr[start];
-        arr[end] += '</mark>';
+      let [start, end] = range;
+      arr[start] = '<mark>' + arr[start];
+      arr[end] += '</mark>';
     }
     return arr.join('');
-}
+  }
 
   function findIndeces(text, subText) {
     let arr = [];
     const m = text.length;
     const n = subText.length;
-    for(let i = 0; i < m; i++) {
-      let num = text.substring(i, i+n).indexOf(subText);
-      if(num != -1) arr.push([i + num, i + num + n-1]);
+    for (let i = 0; i < m; i++) {
+      let num = text.substring(i, i + n).indexOf(subText);
+      if (num != -1) arr.push([i + num, i + num + n - 1]);
     }
     return arr;
   }
@@ -71,15 +71,19 @@ const Note = ({ title, text, index, editText, deleteFun, bgColor, editColor, hig
       <div className='note' style={{ backgroundColor: bgColor }}>
         <h3 ref={titleRef}></h3>
         <div className='note-body' ref={textBoxRef}></div>
-        <div className='icon-container'>
-          <span className='edit-icon-wrapper icon-wrapper' index={index} ref={editSpanRef} onClick={() => editText(editSpanRef)}>
-            <MdOutlineEdit />
-          </span>
-          <span className='delete-icon-wrapper icon-wrapper' index={index} ref={deleteSpanRef} onClick={handleDelete}>
-            <RiDeleteBin5Line />
-          </span>
-          <span className='color-icon-wrapper icon-wrapper' index={index}><ColorPallet noteColor={noteColor} index={index} /></span>
+        <div
+         style={{"height": "33px"}}
+         >
+          <div className='icon-container'>
+            <span className='edit-icon-wrapper icon-wrapper' index={index} ref={editSpanRef} onClick={() => editText(editSpanRef)}>
+              <MdOutlineEdit />
+            </span>
+            <span className='delete-icon-wrapper icon-wrapper' index={index} ref={deleteSpanRef} onClick={handleDelete}>
+              <RiDeleteBin5Line />
+            </span>
+            <span className='color-icon-wrapper icon-wrapper' index={index}><ColorPallet noteColor={noteColor} index={index} /></span>
           </div>
+        </div>
       </div>
       <Modal maskClosable={false} open={showModal} onOk={handleOk} onCancel={handleCancel} okType='danger' okText='Yes' cancelText='No'>
         <label style={{ fontSize: '1.1rem', fontWeight: '600' }}><BsExclamationSquareFill className='exclamation-icon' />Are you sure delete this Note</label>
